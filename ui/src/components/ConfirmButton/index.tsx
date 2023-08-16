2091301a6b237cb0241a089ce7c5f1a9ad0bba19
officialrobert <officialrobert.espina9627@gmail.com>
2023-08-16 19:52:12 +0800

    Confirm button style

--- ui/src/components/ConfirmButton/index.tsx
+++ ui/src/components/ConfirmButton/index.tsx
@@ -1,3 +1,4 @@
+import cx from 'classnames';
 import { Button } from '@mui/material';
 import { isEmpty } from 'lodash';
 import { CSSProperties, ReactNode } from 'react';
@@ -15,29 +16,33 @@ type ConfirmButtonProps = {
 	ariaLabel?: string;
 	onClick?: <T>(props: T) => any | undefined;
 	style?: Partial<CSSProperties>;
+	className?: string;
 	variant?: 'text' | 'contained' | 'outlined';
 };
 
+const btnStyles: Partial<CSSProperties> = {
+	height: '46px',
+	boxSizing: 'border-box',
+	textTransform: 'none',
+	borderRadius: '8px',
+	background: '#345FFF',
+};
+
 const ConfirmButton = (props: ConfirmButtonProps) => {
-	const { isLoading, type, text, variant, disableRipple, children, onClick, ariaLabel, style } = props;
+	const { className, isLoading, type, text, variant, disableRipple, children, onClick, ariaLabel, style } = props;
 
 	if (isLoading) {
 		return (
 			<LoadingButton
 				loading
 				style={{
-					height: '46px',
-					boxSizing: 'border-box',
-					textTransform: 'none',
-					borderRadius: '8px',
-					background: '#345FFF',
-					fontSize: '16px',
+					...btnStyles,
 					...styleCenterElement,
 					...style,
 				}}
 				variant='outlined'
 				aria-label={ariaLabel}
-				className='LoadingButtonSubmit'
+				className={cx('LoadingButtonSubmit', 'ConfirmButton', { [`${className}`]: !isEmpty(className) })}
 			/>
 		);
 	}
@@ -48,17 +53,13 @@ const ConfirmButton = (props: ConfirmButtonProps) => {
 			disableRipple={disableRipple}
 			type={type}
 			style={{
-				height: '46px',
-				boxSizing: 'border-box',
-				textTransform: 'none',
-				borderRadius: '8px',
-				background: '#345FFF',
-				fontSize: '16px',
+				...btnStyles,
 				...styleCenterElement,
 				...style,
 			}}
 			onClick={onClick}
 			{...(!isEmpty(ariaLabel) && { 'aria-label': ariaLabel })}
+			className={cx('ConfirmButton', { [`${className}`]: !isEmpty(className) })}
 		>
 			{children}
 			{text || ''}
